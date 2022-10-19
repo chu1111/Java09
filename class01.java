9f72f8e53eaa654ca86c5f48a8c926847b8d0f74
chu1111 <113088657+chu1111@users.noreply.github.com>
2022-10-19 09:13:27 +0800

    Add files via upload

---
+++ class01.java
@@ -0,0 +1,27 @@
+package java09;
+
+public class class01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class CRectangle{
+	//建立變數
+	int width;
+	int height;
+	CRectangle(int w, int h){
+		//變數設定為參數
+		width = w;
+		height = h;
+	}
+	CRectangle(){
+		width = 0;
+		height = 8;
+		/*c小題
+		this.width = 0
+		this.height = 8*/
+	}
+}
